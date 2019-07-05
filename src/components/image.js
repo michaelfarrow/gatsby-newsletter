727adba5ef51d1c13e4f953d4bd439ec791e3c31
Mike Farrow <contact@mikefarrow.co.uk>
2019-07-05 09:33:19 +0100

    cleaning up

--- src/components/image.js
+++ src/components/image.js
@@ -1,32 +1,38 @@
-import React from "react"
-import { useStaticQuery, graphql } from "gatsby"
-import Img from "gatsby-image"
+import React from 'react'
+import { useStaticQuery, graphql } from 'gatsby'
+import Img from 'gatsby-image'
 
-/*
- * This component is built using `gatsby-image` to automatically serve optimized
- * images with lazy loading and reduced file sizes. The image is loaded using a
- * `useStaticQuery`, which allows us to load the image from directly within this
- * component, rather than having to pass the image data down from pages.
- *
- * For more information, see the docs:
- * - `gatsby-image`: https://gatsby.dev/gatsby-image
- * - `useStaticQuery`: https://www.gatsbyjs.org/docs/use-static-query/
- */
+const Image = props => {
+  const { src, ...otherProps } = props
 
-const Image = () => {
   const data = useStaticQuery(graphql`
     query {
-      placeholderImage: file(relativePath: { eq: "gatsby-astronaut.png" }) {
-        childImageSharp {
-          fluid(maxWidth: 300) {
-            ...GatsbyImageSharpFluid
+      images: allFile {
+        edges {
+          node {
+            relativePath
+            name
+            childImageSharp {
+              fluid(maxWidth: 700) {
+                ...GatsbyImageSharpFluid
+              }
+            }
           }
         }
       }
     }
   `)
 
-  return <Img fluid={data.placeholderImage.childImageSharp.fluid} />
+  const image = data.images.edges.find(edge => {
+    console.log(edge.node.relativePath)
+    return edge.node.relativePath === src
+  })
+
+  if (!image) {
+    return null
+  }
+
+  return <Img {...otherProps} fluid={image.node.childImageSharp.fluid} />
 }
 
 export default Image
