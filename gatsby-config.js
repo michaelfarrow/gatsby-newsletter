bd307a5000a22dbdda96333527bbaa33a27bc96c
weyforth <contact@mikefarrow.co.uk>
2019-07-06 13:05:54 +0100

    fixing typo

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,7 +23,7 @@ module.exports = {
       options: {
         name: 'gatsby-starter-default',
         short_name: 'starter',
-        start_url: `/${PATH_PREFIX.length ? `${PATH_PREFIX}/`,
+        start_url: `/${PATH_PREFIX.length ? `${PATH_PREFIX}/` : '',
         background_color: '#663399',
         theme_color: '#663399',
         display: 'minimal-ui',
